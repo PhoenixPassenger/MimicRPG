74abaa15d4ff912c4797ba48ae8f57b7709e57b2
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-09-09 08:45:50 -0300

    Initial Commit

---
+++ MimicRPG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MimicRPG
+//
+//  Created by Rodrigo Silva Ribeiro on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
