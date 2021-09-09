720878447ab0cb6fe2f3aca127a18fc09568cccf
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-09-09 09:07:22 -0300

    first commit

---
+++ MimicRPG/Helpers/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  MimicRPG
+//
+//  Created by Rodrigo Silva Ribeiro on 09/09/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let rootNavigationController = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = rootNavigationController
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
