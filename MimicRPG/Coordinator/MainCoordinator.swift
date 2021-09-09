8d5f58fb713e98b85de2a65d5227331b6be80a83
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-09-09 09:27:56 -0300

    folder organization

---
+++ MimicRPG/Coordinator/MainCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Main.swift
+//  MimicRPG
+//
+//  Created by Rodrigo Silva Ribeiro on 09/09/21.
+//
+
+import Foundation
---
+++ MimicRPG/Services/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  MimicRPG
+//
+//  Created by Rodrigo Silva Ribeiro on 09/09/21.
+//
+
+import Foundation
