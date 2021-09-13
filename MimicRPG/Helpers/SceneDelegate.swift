a63ab14679ce0507283834c97900ecd52053b86d
Pedro Henrique Costa <henriquepcnews@gmail.com>
2021-09-13 09:19:41 -0300

    - Changes ViewController into TabBarViewController - Changes SceneDelegate to use TabBarViewController - Adds UserSheetsViewController, DiceRollerViewController and SettingsViewController

--- MimicRPG/Helpers/SceneDelegate.swift
+++ MimicRPG/Helpers/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let rootNavigationController = UINavigationController(rootViewController: ViewController())
+        let rootNavigationController = UINavigationController(rootViewController: TabBarViewController())
         self.window?.rootViewController = rootNavigationController
         self.window?.makeKeyAndVisible()
     }
---
+++ MimicRPG/View/TabBarViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MimicRPG
+//
+//  Created by Rodrigo Silva Ribeiro on 09/09/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+        
+        let sheetsViewController = UserSheetsViewController()
+        sheetsViewController.title = "Fichas"
+        sheetsViewController.tabBarItem = UITabBarItem(title: "Fichas", image: UIImage(named: "pencil"),
+                                       selectedImage: UIImage(named: "pencil"))
+        
+        let diceRollerViewController = DiceRollerViewController()
+        diceRollerViewController.title = "Dados"
+        diceRollerViewController.tabBarItem = UITabBarItem(title: "Dados", image: UIImage(named: "pencil"),
+                                                           selectedImage: UIImage(named: "pencil"))
+        
+        let settingsViewController = SettingsViewController()
+        settingsViewController.title = "Opções"
+        settingsViewController.tabBarItem = UITabBarItem(title: "Opções", image: UIImage(named: "gearshape"),
+                                                         selectedImage: UIImage(named: "gearshape.fill"))
+
+        let sheetsNavigationController = UINavigationController(rootViewController: sheetsViewController)
+        let diceNavigationController = UINavigationController(rootViewController: diceRollerViewController)
+        let settingsNavigationController = UINavigationController(rootViewController: settingsViewController)
+        self.viewControllers = [sheetsNavigationController, diceNavigationController, settingsNavigationController]
+    }
+
+}
