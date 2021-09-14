7b14c4d9dd0bf781567dff6d3894cf16d3cbb626
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2021-09-14 10:00:37 -0300

    localized strings e configurações de multi lingua

--- MimicRPG/View/ViewController.swift
+++ MimicRPG/View/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .purple
+        self.label.text = LocalizedStrings.hello.localized
+        self.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
 
 }
