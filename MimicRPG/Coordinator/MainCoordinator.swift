d8bfdf0e5188c4d17de46b2258db0a48651c2c38
Pedro Henrique Costa <henriquepcnews@gmail.com>
2021-09-14 10:35:01 -0300

    - Adds fodendo MainCoordinator

--- MimicRPG/Coordinator/MainCoordinator.swift
+++ MimicRPG/Coordinator/MainCoordinator.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import UIKit
+
+class MainCoordinator : Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let startingViewController = TabBarViewController()
+        startingViewController.coordinator = self
+
+        let sheetsViewController = UserSheetsViewController()
+        sheetsViewController.coordinator = self
+        sheetsViewController.title = "Fichas"
+        sheetsViewController.tabBarItem = UITabBarItem(title: "Fichas", image: UIImage(named: "pencil"),
+                                       selectedImage: UIImage(named: "pencil"))
+
+        let diceRollerViewController = DiceRollerViewController()
+        diceRollerViewController.coordinator = self
+        diceRollerViewController.title = "Dados"
+        diceRollerViewController.tabBarItem = UITabBarItem(title: "Dados", image: UIImage(named: "pencil"),
+                                                           selectedImage: UIImage(named: "pencil"))
+
+        let settingsViewController = SettingsViewController()
+        settingsViewController.coordinator = self
+        settingsViewController.title = "Opções"
+        settingsViewController.tabBarItem = UITabBarItem(title: "Opções", image: UIImage(named: "gearshape"),
+                                                         selectedImage: UIImage(named: "gearshape.fill"))
+
+        let sheetsNavigationController = UINavigationController(rootViewController: sheetsViewController)
+        let diceNavigationController = UINavigationController(rootViewController: diceRollerViewController)
+        let settingsNavigationController = UINavigationController(rootViewController: settingsViewController)
+
+        startingViewController.viewControllers = [sheetsNavigationController, diceNavigationController,
+                                                  settingsNavigationController]
+
+        navigationController.pushViewController(startingViewController, animated: false)
+    }
+
+    func goToSelectedSheet(from controller: UIViewController) {
+        let viewController = DisplaySheetViewController()
+        
+    }
+}
