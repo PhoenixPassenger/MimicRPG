b89c353a833fb0464d6adad611c9d4ca6def3b49
Eduardo Regis <eduregis1@gmail.com>
2021-10-04 10:03:45 -0300

    calling a modal

--- MimicRPG/View/UserSheetsViewController.swift
+++ MimicRPG/View/UserSheetsViewController.swift
@@ -101,7 +101,7 @@ class UserSheetsViewController: UIViewController, UISearchResultsUpdating {
 //        newSheetModal.viewModel = NewSheetModalViewModel()
 //        newSheetModal.viewModel.output = newSheetModal
 //        present(newSheetModal, animated: true, completion: nil)
-        let createSheetModal = CreateSheetModal()
+        let createSheetModal = CreateAttackModal()
         present(createSheetModal, animated: true, completion: nil)
     }
 
